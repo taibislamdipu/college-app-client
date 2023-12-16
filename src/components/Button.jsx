64ab8dbe1d46e90fd80e8dfaf854cc1a46bccc6c
taibislamdipu <mailtaibislam@gmail.com>
2023-12-16 13:06:16 +0600

    on progress..

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,5 @@
 import { AiFillCaretRight } from "react-icons/ai";
 
-/* eslint-disable react/prop-types */
 const Button = ({ btnText }) => {
   return (
     <button className="py-3 px-4 text-center flex items-center justify-center gap-2 bg-primary text-white rounded font-medium hover:bg-white hover:text-primary transition w-full custom-box-shadow">
